78b9e8709945b7d53fbe2fb2b2c4deb219e85625
jteib <jteib@yahoo.com>
2019-07-11 11:41:03 -0400

    Converted to use react-redux new Hooks api

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
@@ -7,35 +7,48 @@ import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
-import { sortCourses } from "./CoursesPage";
 
-const ManageCoursePage = ({
-  courses,
-  authors,
-  loadAuthors,
-  loadCourses,
-  saveCourse,
-  history,
-  ...props //Rest operator-this says "Assign any props I havent destructured above to a variable called props"
-}) => {
-  const [course, setCourse] = useState({ ...props.course });
+const ManageCoursePage = ({ history, match }) => {
+  const dispatch = useDispatch();
+  const redux = useSelector(state => {
+    const slug = match.params.slug;
+
+    function getCourseBySlug(courses, slug) {
+      return courses.find(course => course.slug === slug) || null; //called a selector.  It selects data from the Redux store. Could also be placed in courseReducer for re-use. for performance, memoize with reselect.
+    }
+
+    const course =
+      slug && state.courses.length > 0
+        ? getCourseBySlug(state.courses, slug)
+        : newCourse;
+    return {
+      course, //removed newCourse as it's now defined above as const course depending on ternary operation
+      courses: state.courses,
+      authors: state.authors,
+      loading: state.apiCallsInProgress > 0
+    };
+  });
+
+  const authors = redux.authors;
+  const [course, setCourse] = useState({ ...redux.course });
   const [errors, setErrors] = useState({});
   const [saving, setSaving] = useState(false);
+
   useEffect(() => {
-    if (courses.length === 0) {
-      loadCourses().catch(error => {
+    if (redux.courses.length === 0) {
+      dispatch(loadCourses()).catch(error => {
         alert("Loading courses failed" + error);
       });
     } else {
-      setCourse({ ...props.course });
+      setCourse({ course });
     }
 
-    if (authors.length === 0) {
-      loadAuthors().catch(error => {
+    if (redux.authors.length === 0) {
+      dispatch(loadAuthors()).catch(error => {
         alert("Loading authors failed" + error);
       });
     }
-  }, [props.course]);
+  }, [course]);
 
   function handleChange(event) {
     const { name, value } = event.target; //destructuring here
@@ -62,7 +75,7 @@ const ManageCoursePage = ({
     event.preventDefault();
     if (!formIsValid()) return;
     setSaving(true);
-    saveCourse(course)
+    dispatch(saveCourse(course))
       .then(() => {
         toast.success("Course Saved");
         history.push("/courses");
@@ -73,7 +86,7 @@ const ManageCoursePage = ({
       });
   }
 
-  return authors.length === 0 && courses.length === 0 ? (
+  return redux.authors.length === 0 && redux.courses.length === 0 ? (
     <Spinner />
   ) : (
     <CourseForm
@@ -88,41 +101,8 @@ const ManageCoursePage = ({
 };
 
 ManageCoursePage.propTypes = {
-  course: PropTypes.object.isRequired,
-  authors: PropTypes.array.isRequired,
-  courses: PropTypes.array.isRequired,
-  loadCourses: PropTypes.func.isRequired,
-  loadAuthors: PropTypes.func.isRequired,
-  saveCourse: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  match: PropTypes.object.isRequired
 };
 
-export function getCourseBySlug(courses, slug) {
-  return courses.find(course => course.slug === slug) || null; //called a selector.  It selects data from the Redux store. Could also be placed in courseReducer for re-use. for performance, memoize with reselect.
-}
-
-function mapStateToProps(state, ownProps /*automatically populated by redux*/) {
-  const slug = ownProps.match.params.slug;
-  const course =
-    slug && state.courses.length > 0
-      ? getCourseBySlug(state.courses, slug)
-      : newCourse;
-  return {
-    course, //removed newCourse as it's now defined above as const course depending on ternary operation
-    courses: state.courses,
-    authors: state.authors,
-    loading: state.apiCallsInProgress > 0
-  };
-}
-
-const mapDispatchToProps = {
-  loadCourses,
-  loadAuthors,
-  saveCourse
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ManageCoursePage);
+export default ManageCoursePage;
